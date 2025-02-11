27db80fd406daf6347755745b7ac1ae70b4af1e5
Thevenin76 <davidyanesfarina@gmail.com>
2025-02-11 11:46:55 +0000

    Se añade condicional para ver si es admin

--- template.py
+++ template.py
@@ -1,3 +1,7 @@
+'''
+Ejercicio:
+1. Agrega un if en el template que muestre "Eres administrador" si el usuario es "admin".
+'''
 from flask import Flask
 from flask import render_template
 
@@ -5,6 +9,10 @@ app = Flask(__name__)
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
+
+    if (nombre == 'admin'):
+        return render_template('index.html', nombre=nombre, eres_admin='eres_admin')
+     
     return render_template('index.html', nombre=nombre)
 
 if __name__ == '__main__':
